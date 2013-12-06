629db05546942849f3b16700f6bf9a2b4c2c21bb
Raynos <raynos2@gmail.com>
2013-12-05 22:06:00 -0800

    tail supports an object

--- examples/tail/server.js
+++ examples/tail/server.js
@@ -7,7 +7,8 @@ var setTimeout = require("timers").setTimeout
 // configure the Tail so that it does your own error handling
 // in this case we log things
 var tail = Tail(function (err, meta) {
-    console.log("error happened", err.message, meta.name, meta.req.url)
+    console.log("error happened %s on uri %s for event %s with key %s", 
+        err.message, meta.req.url, meta.name, meta.key)
 })
 var app = Router()
 
@@ -27,7 +28,11 @@ var fakeCache = {
 // however we do want to configure error handling in one place
 // and have the error from the async thing be reported properly
 app.addRoute("/:key/:value", function (req, res, opts) {
-    var callback = tail("fakeCache", req, res)
+    var callback = tail("fakeCache", {
+        req: req,
+        res: res,
+        key: opts.key
+    })
     fakeCache.update(opts.key, opts.value, callback)
 
     sendText(req, res, "successfully saved thing")
@@ -38,16 +43,14 @@ server.listen(3000)
 console.log("tail server listening on port 3000")
 
 function Tail(errorHandler) {
-    function tail(name, req, res) {
+    function tail(name, opts) {
+        opts.name = name
+
         return callback
 
         function callback(err) {
             if (err) {
-                errorHandler(err, {
-                    name: name,
-                    req: req,
-                    res: res
-                })
+                errorHandler(err, opts)
             }
         }
     }
