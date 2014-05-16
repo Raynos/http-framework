f6a5b21971463ea9f6021f60b512e16177aeb9ff
jared <jaredhalpert90@gmail.com>
2014-05-16 01:04:30 -0700

    tightened up serve-favicon

--- examples/favicon/server.js
+++ examples/favicon/server.js
@@ -1,18 +1,20 @@
 // based on https://github.com/expressjs/serve-favicon#vanilla-http-server
 // middleware for serving a favicon using vanilla http
 var http = require('http')
-  , favicon = require('serve-favicon');
+  , path = require('path')
+  , favicon = require('serve-favicon')
 
-var _favicon = favicon(__dirname+'/favicon.ico');
+var _favicon = favicon(path.join(__dirname,'/favicon.ico'))
 
 http.createServer(function(req, res) {
   _favicon(req, res, function(err) {
     if (err) {
-      res.statusCode = 500;
-      res.end('error');
+      res.statusCode = 500
+      res.end('error')
+      return
     }
-    res.end('favicon example');
-  });
+    res.end('favicon example')
+  })
 }).listen(3000, function() {
-  console.log('listening on port: 3000');
-});
+  console.log('listening on port: 3000')
+})
